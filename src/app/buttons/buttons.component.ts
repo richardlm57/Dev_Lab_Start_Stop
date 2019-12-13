09f8d001904efca97d148222d60cf99f4806cf90
JhosuaArias <jhosuarqui@hotmail.com>
2019-12-13 22:25:19 +0000

    Buttons

--- src/app/buttons/buttons.component.ts
+++ src/app/buttons/buttons.component.ts
@@ -9,15 +9,16 @@ export class ButtonsComponent implements OnInit {
 
   private startUrl = 'https://startstopj.azurewebsites.net/api/Start?code=QTunwb7Pe2h6ccOn4agxjhkvnNiskljWuEiDpHOar5rVagXYzbhfDA==';
   private stopUrl = 'https://startstopj.azurewebsites.net/api/Stop?code=J6pbZ7O6T8ToM9e7uszaqyMmurSYfkoNwDoefgZHhecKmgjaFS2Lbg==';
+  private getUrl = 'https://startstopj.azurewebsites.net/api/GetVM?code=SNookVpk4j/6Ru4jR8swBiegQC60Cay0/c8lOFkzn/GUgwk1yGS9Tw==';
 
-  private VMInfo = {vmname: 'LinuxTest', resourcegroup: 'JhosuaAriasRG'};
+  private VMInfo = {vmname: 'VMTesting', resourcegroup: 'FEDP-RG'};
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
   startVM() {
-    this.http.post(this.startUrl, this.VMInfo).subscribe(something => console.log(something));
+    this.http.post(this.getUrl, this.VMInfo).subscribe(something => console.log(something));
     console.log("Start");
   }
 
